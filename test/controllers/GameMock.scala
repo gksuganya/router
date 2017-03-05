70574a0e13fa6024b9bbb3a6ac7c3deefdb51dd0
Alex Collins <alex.e.c@gmail.com>
2017-03-05 16:54:08 +0000

    configure

--- test/controllers/GameMock.scala
+++ test/controllers/GameMock.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import com.github.tomakehurst.wiremock.WireMockServer
-import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.client.WireMock.{configureFor, reset}
 import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, Suite}
 
 trait GameMock extends BeforeAndAfterAll with BeforeAndAfter {
@@ -13,7 +13,8 @@ trait GameMock extends BeforeAndAfterAll with BeforeAndAfter {
   override def beforeAll(): Unit = gameMock.start()
 
   before {
-    WireMock.reset()
+    configureFor(gamePort)
+    reset()
   }
 
   override def afterAll(): Unit = gameMock.stop()
